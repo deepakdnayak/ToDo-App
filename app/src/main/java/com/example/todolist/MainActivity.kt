e39ae55efe653283ea8f49f5129c27693981c112
deepakdnayak <deepakdnayak2004@gmail.com>
2024-09-09 23:42:39 +0530

    Completed Basic App

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,31 +1,40 @@
 package com.example.todolist
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
 
     private val tasks = mutableListOf<String>()
     private lateinit var adapter: TaskAdapter
+    private val sharedPrefFile = "com.example.todolist.PREFERENCE_FILE_KEY"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val addButton = findViewById<Button>(R.id.addButton)
+        val addButtonW = findViewById<ImageButton>(R.id.addButtonW)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewTasks)
 
-        adapter = TaskAdapter(tasks)
+        adapter = TaskAdapter(tasks) {position -> deleteTask(position)}
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
 
-        addButton.setOnClickListener{
+        loadTasks()
+
+        addButtonW.setOnClickListener {
             showAddTaskDailogue()
         }
     }
@@ -37,7 +46,7 @@ class MainActivity : AppCompatActivity() {
             .setView(dailogueView)
             .setCancelable(true)
 
-        val alertDailog = dailogueBuilder.create()
+        val alertDailogue = dailogueBuilder.create()
 
         val saveButton = dailogueView.findViewById<Button>(R.id.saveTaskButton)
         val taskInput = dailogueView.findViewById<EditText>(R.id.editTextTask)
@@ -47,9 +56,42 @@ class MainActivity : AppCompatActivity() {
             if(task.isNotEmpty()){
                 tasks.add(task)
                 adapter.notifyItemInserted(tasks.size - 1)
-                alertDailog.dismiss()
+                alertDailogue.dismiss()
             }
+            saveTasks()
         }
-        alertDailog.show()
+        alertDailogue.show()
+    }
+
+    private fun deleteTask(position: Int) {
+        tasks.removeAt(position)
+        adapter.notifyItemRemoved(position)
+        saveTasks()
+    }
+
+    private fun saveTasks() {
+        val sharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        val gson = Gson()
+        val jsonTasks = gson.toJson(tasks)
+
+        editor.putString("TASKS_KEY", jsonTasks)
+        editor.apply()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun loadTasks() {
+        val sharedPreferences = getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val jsonTasks = sharedPreferences.getString("TASKS_KEY",null)
+
+        if(!jsonTasks.isNullOrEmpty()) {
+            val gson = Gson()
+            val type = object : TypeToken<List<String>>() {}.type
+            val loadedTasks : List<String> = gson.fromJson(jsonTasks, type)
+
+            tasks.addAll(loadedTasks)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/example/todolist/TaskAdapter.kt
+++ app/src/main/java/com/example/todolist/TaskAdapter.kt
@@ -3,13 +3,18 @@ package com.example.todolist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskAdapter(private val tasks: List<String>): RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+class TaskAdapter(
+    private val tasks: List<String>,
+    private val onDeleteClick: (Int) -> Unit
+): RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
     class TaskViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val taskTextView: TextView = itemView.findViewById(R.id.taskText)
+        val deleteButton: ImageButton = itemView.findViewById(R.id.deleteButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -19,6 +24,10 @@ class TaskAdapter(private val tasks: List<String>): RecyclerView.Adapter<TaskAda
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         holder.taskTextView.text = tasks[position]
+
+        holder.deleteButton.setOnClickListener{
+            onDeleteClick(position)
+        }
     }
 
     override fun getItemCount(): Int {
